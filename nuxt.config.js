9793440943d73a7858ba75240eb90e0073f2f481
Maya Shavin <maya@cloudinary.com>
2020-09-04 00:09:19 +0300

    fix(doc): update social media card

--- nuxt.config.js
+++ nuxt.config.js
@@ -25,7 +25,7 @@ export default {
         hid: 'twitter:image',
         name: 'twitter:image',
         content:
-          'https://res.cloudinary.com/mayashavin/image/upload/q_auto,f_auto/v1599115565/colors_generator/logo_transparent_background'
+          'https://res.cloudinary.com/mayashavin/image/upload/q_auto,f_auto/v1599115565/colors_generator/palette-generator'
       },
       // Twitter Card
       {
@@ -63,7 +63,7 @@ export default {
         hid: 'og:image',
         property: 'og:image',
         content:
-          'https://res.cloudinary.com/mayashavin/image/upload/q_auto,f_auto/v1599115565/colors_generator/logo_transparent_background'
+          'https://res.cloudinary.com/mayashavin/image/upload/q_auto,f_auto/v1599115565/colors_generator/palette-generator'
       }
     ],
     link: [
